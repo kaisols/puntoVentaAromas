232567f2dbdf3480ef275001b9d17f8e77f76dbc
Kaina Solano Soto <130873413+kaisols@users.noreply.github.com>
2024-07-15 19:27:36 -0600

    Entregable#4
    Mantenimiento de categorias

---
+++ App_PV_AromasModels/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_PV_Aromas.Models
+{
+    public class Categoria 
+    {
+        #region ============================= PROPIEDADES =============================
+
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public bool estado { get; set; }
+        public int? tipoconsulta { get; set; }
+        public Auditoria? miAuditoria { get; set; }
+
+
+        #endregion ============================= PROPIEDADES =============================
+    }
+}
