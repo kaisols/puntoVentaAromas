f343e6942b17b3c8abb2671f6af3a10eae9f81de
Kaina Solano Soto <130873413+kaisols@users.noreply.github.com>
2024-07-19 18:23:17 -0600

    entregable #4
    entrega #4

---
+++ App_PV_AromasModels/Proveedor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_PV_Aromas.Models
+{
+    public class Proveedor
+    {
+
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public string? cedula { get; set; }
+        public string? telefono { get; set; }
+        public string? correo { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public bool estado { get; set; }
+        public int? tipoconsulta { get; set; }
+        public Auditoria? miAuditoria { get; set; }
+    }
+}
