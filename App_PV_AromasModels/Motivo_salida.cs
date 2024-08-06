708ecee590f7d3037b33df19a28201596d9bf01e
Kaina Solano Soto <130873413+kaisols@users.noreply.github.com>
2024-08-05 19:47:00 -0600

    Entregable #8
    Entregable #8

---
+++ App_PV_AromasModels/Motivo_salida.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_PV_Aromas.Models
+{
+    public class Motivo_salida  
+    {
+        #region ============================= PROPIEDADES =============================
+
+        public int id { get; set; }
+        public string? nombre { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public bool estado { get; set; }
+        public int? tipoconsulta { get; set; }
+        public Auditoria? miAuditoria { get; set; }
+
+        #endregion ============================= PROPIEDADES =============================
+    }
+}
