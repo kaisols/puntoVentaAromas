c8f07d4c6eac7b9ea20c6f9e9815311438250de9
Kaina Solano Soto <130873413+kaisols@users.noreply.github.com>
2024-08-06 10:26:56 -0600

    Entregable #9
    Entregable #9

---
+++ App_PV_AromasModels/Salidainventario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_PV_Aromas.Models
+{
+    public class Salidainventario  
+    {
+        #region ============================= PROPIEDADES =============================
+
+        public int id { get; set; }
+        public Motivo_salida? miMotivo { get; set; }
+        public Usuario? miUsuario { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public bool estado { get; set; }
+        public int? tipoconsulta { get; set; }
+        public Auditoria? miAuditoria { get; set; }
+        public List<Salidainventario_detalle>? Detalles { get; set; }
+
+        #endregion ============================= PROPIEDADES =============================
+    }
+}
