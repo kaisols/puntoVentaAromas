84801067b3ea5ffbe997505471cef2f3efa53498
Kaina Solano Soto <130873413+kaisols@users.noreply.github.com>
2024-08-26 17:20:04 -0600

    Entregable #10
    Entregable #10

---
+++ App_PV_AromasModels/Factura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_PV_Aromas.Models
+{
+    public class Factura
+    {
+        #region ============================= PROPIEDADES =============================
+
+        public int id { get; set; }
+        public Usuario? miUsuario { get; set; }
+        public Cliente? miCliente { get; set; }
+        public Tipofactura? miTipoFactura { get; set; }
+        public decimal subtotal { get; set; }
+        public decimal montoTotal { get; set; }
+        public decimal ivaTotal { get; set; }
+        public decimal descuento { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public bool estado { get; set; }
+        public int? tipoconsulta { get; set; }
+        public Auditoria? miAuditoria { get; set; }
+        public List<Factura_Detalle>? Detalles { get; set; }
+
+        #endregion ============================= PROPIEDADES =============================
+    }
+}
---
+++ App_PV_AromasModels/Factura_Detalle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace App_PV_Aromas.Models
+{
+    public class Factura_Detalle
+    {
+        #region ============================= PROPIEDADES =============================
+
+        public int id { get; set; }
+        public Factura miFactura { get; set; }
+        public Productos miProducto { get; set; }
+        public decimal cantidad { get; set; }
+        public decimal subtotal { get; set; }
+        public decimal montoTotal { get; set; }
+        public decimal ivaTotal { get; set; }
+        public decimal descuento { get; set; }
+        public DateTime fecha_registro { get; set; }
+        public bool estado { get; set; }
+        public int? tipoconsulta { get; set; }
+        public Auditoria? miAuditoria { get; set; }
+        
+        [JsonIgnore]
+        public int Porcentaje_Descuento { get; set; }
+
+        #endregion ============================= PROPIEDADES =============================
+
+    }
+}
